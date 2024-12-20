7a8119080acbcdf50146fe13310f980b47ccb1aa
YuYeaJin <yyj9290@naver.com>
2024-12-20 13:00:52 +0900

    스타일드 컴포넌트

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import StyledComponents from "./styled/styledComponents.jsx";
+
+function App() {
+  return (
+    <div>
+      <h1>StyledComponents</h1>
+      <StyledComponents />
+    </div>
+  );
+}
+
+export default App;
